425544227b8649f1192ea0207a809253f88d77ef
ffelipegupe <1560@holbertonschool.com>
2020-07-24 00:40:02 +0000

    Add: adding task 4 file

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - function that checks if a node is a leaf
+ * @tree: pointer to the root node of the tree to delete
+ *
+ * Return: 1, if node is a lead; 0, otherwise or if node equals NULL
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node && (!node->left && !node->right))
+		return (1);
+	return (0);
+}
