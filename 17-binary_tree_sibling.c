a0515eee21ac41563a0c7e6edf4821e08bfabbd8
ffelipegupe <1560@holbertonschool.com>
2020-07-24 03:49:14 +0000

    Add: adding task 17 files

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - function that counts the leaves in a  binary tree
+ * @node: pointer to the node to find the sibling
+ *
+ * Return: a pointer to the sibling node; NULL, if node or the parent is NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left != node)
+		return (node->parent->left);
+	if (node->parent->right != node)
+		return (node->parent->right);
+	return (NULL);
+}
