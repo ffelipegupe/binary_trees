79602be57531c80567bf49334f5797860b03405b
ffelipegupe <1560@holbertonschool.com>
2020-07-24 01:46:32 +0000

    Edit: styling

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depht - function that measures the depth of a node
+ * binary_tree_depth - function that measures the depth of a node
  * @tree: pointer to the node to measure the depht
  *
  * Return: 0, if tree is NULL.
