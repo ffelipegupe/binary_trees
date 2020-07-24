35ebc69c517121ecea968c08add14c0e79cc34be
ffelipegupe <1560@holbertonschool.com>
2020-07-24 01:14:29 +0000

    Edit: edting function

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,7 +6,7 @@
  *
  * Return: nothing
  */
-void binary_tree_postrder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
