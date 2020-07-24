05b35e2e25b83c4cf16505494d5ca19c25d47888
ffelipegupe <1560@holbertonschool.com>
2020-07-24 01:31:22 +0000

    Add: adding task 10 files

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,7 +7,7 @@
  */
 size_t binary_tree_depht(const binary_tree_t *tree)
 {
-	if (!tree || !tree->parent)
+	if ((!tree) || (!tree->parent))
 		return (0);
 	return (binary_tree_depth(tree->parent));
 }
