4a37400bbe4f3599137f748ab66e0ad44d2576e9
ffelipegupe <1560@holbertonschool.com>
2020-07-24 02:24:43 +0000

    Add: adding task 11 files

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: pointer to the node to measure the depht
+ *
+ * Return: 0, if tree is NULL.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	if (!tree->left && !tree->right)
+		return (1);
+	return(binary_tree_size(tree->left) + binary_tree_size(tree_right) + 1);
+}
