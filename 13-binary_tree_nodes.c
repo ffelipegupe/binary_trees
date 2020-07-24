1766b54eab3df33fd98cafc425fcea8c8d13d934
ffelipegupe <1560@holbertonschool.com>
2020-07-24 02:47:42 +0000

    Update: updating function

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,5 +9,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->rigth) + 1);
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
